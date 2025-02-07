73f4bfbbbc0f9815e86cef6af69b70ebce4bfabb
Satyam Barot <satyamtalod@gmai.com>
2025-02-07 22:12:31 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,62 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
+
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    "year": "2024",
+    "role": "Software Developer",
+    "company": "WebCodeGenie Technology Pvt. Ltd.",
+    "description": "Designed optimized database schemas, refactored code to improve maintainability, and implemented caching strategies for enhanced performance. Developed global jQuery and AJAX functions for consistency and scalability.",
+    "technologies": ["Laravel", "MySQL", "JavaScript", "jQuery", "AJAX"]
+  },
+  {
+    "year": "2023 - 2024",
+    "role": "Intern",
+    "company": "Desire Infotech",
+    "description": "Contributed to dynamic web applications using React.js. Assisted with debugging and troubleshooting to ensure optimal performance and seamless user experiences.",
+    "technologies": ["React.js", "JavaScript", "Css", "Html5"]
+  }
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Task Management App",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "Sola Ahmedabad,Gujarat",
+  phoneNo: "+91 9313423713",
+  email: "satyambarot2002@gmail.com",
+};
